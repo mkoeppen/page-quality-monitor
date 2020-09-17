320d34fbb53d2141e12d6981ad0bac5207b09a01
unknown <mkoeppen@posteo.de>
2020-09-17 20:37:36 +0200

    add nuxt

--- server/src/server.js
+++ server/src/server.js
@@ -1,23 +1,47 @@
 'use strict';
-
+const { loadNuxt, build } = require('nuxt');
 const express = require('express');
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const isDev = process.env.NODE_ENV !== 'production'
+const port = 8080;
+const host = '0.0.0.0';
 
 // App
 const app = express();
 
-app.use(express.json())
+// app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send(`PQM Server is running`);
-});
+// app.get('/', (req, res) => {
+//     res.send(`PQM Server is running`);
+// });
 
-app.get('/generate-report/:website', (req, res) => {
-  res.send(`generate: ${req.params.website}`);
-});
+// app.get('/generate-report/:website', (req, res) => {
+//   res.send(`generate: ${req.params.website}`);
+// });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+// app.listen(port, host);
+// console.log(`Running on http://${host}:${port}`);
+
+
+async function start() {
+  // We get Nuxt instance
+  const nuxt = await loadNuxt(isDev ? 'dev' : 'start')
+
+  app.get('/generate-report/:website', (req, res) => {
+    res.send(`generate: ${req.params.website}`);
+  });
+
+  // Render every route with Nuxt.js
+  app.use(nuxt.render)
+
+  // Build only in dev mode with hot-reloading
+  if (isDev) {
+    build(nuxt)
+  }
+  // Listen the server
+  app.listen(port, '0.0.0.0')
+  console.log('Server listening on `localhost:' + port + '`.')
+}
+
+start()
