0971b9b973340cd2bb8850725872182d7d280e96
unknown <mkoeppen@posteo.de>
2020-09-21 20:16:50 +0200

    refactoring

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 const express    = require("express");
-const controller = require("../controllers");
+const automatedTestsController = require("../controllers/automatedTests");
 const routes     = express.Router();
 
-routes.route("/automated-tests").get(controller.getAutomatedTests);
-routes.route("/automated-tests").post(controller.addAutomatedTest);
-routes.route("/automated-tests").put(controller.updateAutomatedTest);
-routes.route("/automated-tests/:id").delete(controller.deleteAutomatedTest);
+routes.route("/automated-tests").get(automatedTestsController.getAutomatedTests);
+routes.route("/automated-tests").post(automatedTestsController.addAutomatedTest);
+routes.route("/automated-tests").put(automatedTestsController.updateAutomatedTest);
+routes.route("/automated-tests/:id").delete(automatedTestsController.deleteAutomatedTest);
 
 module.exports = routes;
--- src/controllers/automatedTests.js
+++ src/controllers/automatedTests.js
@@ -1,4 +1,4 @@
-const model = require("../models");
+const model = require("../models/automatedTests");
 
 /**
  * @function getAutomatedTests
