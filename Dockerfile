d30ace1de89c3016941f508433b72f244e74d78e
Markus Köppen <mkoeppen@posteo.de>
2021-04-25 16:28:50 +0200

    get nuxt running in docker

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,9 @@ WORKDIR /usr/src/app
 # COPY ./package.json /usr/src/app/package.json
 
 COPY ./app /usr/src/app
+RUN rm /usr/src/app/package-lock.json
 RUN npm install
+RUN npm run build
 
 EXPOSE 3000
 EXPOSE 3306
--- app/server/index.js
+++ app/server/index.js
@@ -16,7 +16,10 @@ async function start () {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
-  const { host, port } = nuxt.options.server
+  const {
+    host = process.env.HOST || '127.0.0.1',
+    port = process.env.PORT || 3000
+  } = nuxt.options.server
 
   // Build only in dev mode
   if (config.dev) {
